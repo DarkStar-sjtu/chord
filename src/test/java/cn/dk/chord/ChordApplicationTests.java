ab3be1bd5e04733840501bb5684d4b96eff72942
DarkStar <levy.lu@outlook.com>
2025-05-29 16:39:29 +0800

    Init Project

---
+++ src/test/java/cn/dk/chord/ChordApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.dk.chord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
